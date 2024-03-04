f23aebec91d48f14aae6282391c532fd85d625b7
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-03-03 22:20:49 -0500

    Add items constructor

---
+++ src/item.js
@@ -0,0 +1,8 @@
+class Item {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
