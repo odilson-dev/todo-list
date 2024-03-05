165a36ddd08607b5dd85870ded47c92eff2751aa
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-03-05 14:26:39 -0500

    Loading CSS

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,2 @@
-alert("It's working");
+(()=>{const e=document.getElementById("showDialog"),t=document.getElementById("favDialog"),u=document.querySelector("output"),l=document.querySelector("select"),n=document.querySelector("#confirmBtn");console.log(e),l.addEventListener("change",(e=>{n.value=l.value})),t.addEventListener("close",(e=>{u.value="default"===t.returnValue?"No return value.":`ReturnValue: ${t.returnValue}.`})),n.addEventListener("click",(e=>{e.preventDefault(),t.close(l.value)}))})();
+//# sourceMappingURL=main.js.map
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-alert("It's working");
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("favDialog");
+const outputBox = document.querySelector("output");
+const selectEl = document.querySelector("select");
+const confirmBtn = document.querySelector("#confirmBtn");
+
+// "Show the dialog" button opens the <dialog> modally
+console.log(showButton);
+// showButton.addEventListener("click", () => {
+//   favDialog.showModal();
+// });
+
+// "Favorite animal" input sets the value of the submit button
+selectEl.addEventListener("change", (e) => {
+  confirmBtn.value = selectEl.value;
+});
+
+// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+favDialog.addEventListener("close", (e) => {
+  outputBox.value =
+    favDialog.returnValue === "default"
+      ? "No return value."
+      : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+});
+
+// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // We don't want to submit this fake form
+  favDialog.close(selectEl.value); // Have to send the select box value here.
+});
