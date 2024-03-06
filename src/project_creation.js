d6afcb86722ac8cbc2c81797bb39a915d96ea008
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-03-05 23:20:28 -0500

    Create the project_creation module
    This module will handle all the stuff about creating a new project

---
+++ src/project_creation.js
@@ -0,0 +1,31 @@
+export const handleProjectCreation = function () {
+  const showButton = document.getElementById("showDialog");
+  const favDialog = document.getElementById("favDialog");
+  const outputBox = document.querySelector("output");
+  const selectEl = favDialog.querySelector("select");
+  const confirmBtn = favDialog.querySelector("#confirmBtn");
+
+  // "Show the dialog" button opens the <dialog> modally
+  showButton.addEventListener("click", () => {
+    favDialog.showModal();
+  });
+
+  // "Favorite animal" input sets the value of the submit button
+  selectEl.addEventListener("change", (e) => {
+    confirmBtn.value = selectEl.value;
+  });
+
+  // "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+  favDialog.addEventListener("close", (e) => {
+    outputBox.value =
+      favDialog.returnValue === "default"
+        ? "No return value."
+        : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+  });
+
+  // Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+  confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    favDialog.close(selectEl.value); // Have to send the select box value here.
+  });
+};
