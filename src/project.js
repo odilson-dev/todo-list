9e11340f9885fc35749f238c7210600028c9f13a
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-03-13 15:07:02 -0400

    Remove default todo-list items

--- src/project.js
+++ src/project.js
@@ -3,34 +3,6 @@ export class Project {
   constructor(name) {
     this.name = name;
     this.id = ++Project.idCounter;
-    this.todoList = [
-      {
-        id: 1,
-        title: "Todo",
-        description: "This is a description",
-        dueDate: "2024-03-04",
-        priority: "Hight",
-        note: "This is a small note for the todo",
-        checked: true,
-      },
-      {
-        id: 2,
-        title: "Todo",
-        description: "This is a description",
-        dueDate: "2024-03-04",
-        priority: "Hight",
-        note: "This is a small note for the todo",
-        checked: false,
-      },
-      {
-        id: 3,
-        title: "Todo",
-        description: "This is a description",
-        dueDate: "2024-03-04",
-        priority: "Hight",
-        note: "This is a small note for the todo",
-        checked: true,
-      },
-    ];
+    this.todoList = [];
   }
 }
